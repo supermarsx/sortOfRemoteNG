d6139238c3e4044ecb0ab44fe3a4de0077726ad4
Mariana M <github_618a@imariana.com>
2025-07-13 23:40:57 +0100

    migrate localStorage data to indexeddb

--- src/utils/__tests__/indexedDbService.test.ts
+++ src/utils/__tests__/indexedDbService.test.ts
@@ -10,6 +10,9 @@ beforeEach(async () => {
   await IndexedDbService.init();
   const db = await openDB(DB_NAME, 1);
   await db.clear(STORE_NAME);
+  if (typeof localStorage !== 'undefined') {
+    localStorage.clear();
+  }
 });
 
 describe('IndexedDbService', () => {
@@ -25,5 +28,21 @@ describe('IndexedDbService', () => {
     await db.put(STORE_NAME, 'notjson', 'bad');
     const result = await IndexedDbService.getItem('bad');
     expect(result).toBeNull();
+  });
+
+  it('migrates data from localStorage on init', async () => {
+    const { JSDOM } = await import('jsdom');
+    const dom = new JSDOM('<!doctype html><html><body></body></html>', { url: 'http://localhost' });
+    (global as any).window = dom.window;
+    (global as any).document = dom.window.document;
+    (global as any).localStorage = dom.window.localStorage;
+
+    localStorage.setItem('mremote-test', JSON.stringify({ foo: 'bar' }));
+
+    await IndexedDbService.init();
+
+    const migrated = await IndexedDbService.getItem<{ foo: string }>('mremote-test');
+    expect(migrated).toEqual({ foo: 'bar' });
+    expect(localStorage.getItem('mremote-test')).toBeNull();
   });
 });
