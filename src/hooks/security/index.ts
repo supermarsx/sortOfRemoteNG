89ed11751b60fd7150e9d1a8029dc3a762770ab4
supermarsx <17675589+supermarsx@users.noreply.github.com>
2026-03-03 23:21:19 +0000

    feat(hooks): export new SSH and security hooks from barrel files
    - Re-export useSSHCommandHistory, useSSHAgentManager, useOpkssh,
      useServerStats, useTerminalBackground from ssh/index
    - Re-export useLetsEncryptManager from security/index

--- src/hooks/security/index.ts
+++ src/hooks/security/index.ts
@@ -1,6 +1,7 @@
 export * from "./useAutoLockManager";
 export * from "./useBackupCodesSection";
 export * from "./useCertificateInfoPopup";
+export * from "./useLetsEncryptManager";
 export * from "./usePasswordDialog";
 export * from "./useSecurityQuestionsSection";
 export * from "./useTotpImport";
