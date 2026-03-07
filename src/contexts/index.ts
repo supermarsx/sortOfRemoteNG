c094367e09a5a1496b0fcd720a6653b3b1192805
supermarsx <17675589+supermarsx@users.noreply.github.com>
2026-03-07 13:02:58 +0000

    refactor(exports): add barrel exports for ui and contexts

---
+++ src/contexts/index.ts
@@ -0,0 +1,11 @@
+export { ConnectionProvider } from "./ConnectionProvider";
+export { ConnectionContext } from "./ConnectionContextTypes";
+export type {
+  ConnectionState,
+  ConnectionAction,
+  ConnectionContextType,
+} from "./ConnectionContextTypes";
+export { SettingsProvider, useSettings } from "./SettingsContext";
+export { default as SettingsContext } from "./SettingsContext";
+export { ToastProvider, useToastContext } from "./ToastContext";
+export { useConnections } from "./useConnections";
